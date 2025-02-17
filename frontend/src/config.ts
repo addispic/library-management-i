620af14ad7b3a496720d88531cb6675d1a940aa8
addispic <addispik@gmail.com>
2025-02-17 09:53:06 +0300

    changes

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,6 +1,8 @@
-import {io} from 'socket.io-client'
+import { io } from "socket.io-client";
 // base uri
-export const BASE_URI = "http://localhost:5000";
+// export const BASE_URI = "http://localhost:5000";
+export const BASE_URI = "https://mylibrary-server.onrender.com";
 
 // socket
-export const SOCKET = io("ws://localhost:5000")
+// export const SOCKET = io("ws://localhost:5000")
+export const SOCKET = io("https://mylibrary-server.onrender.com");
